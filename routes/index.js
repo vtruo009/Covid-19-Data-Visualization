5389d3d66717d9028dfd852d456041b2df6e129e
Mari-Hayashi <mhaya010@ucr.edu>
2020-04-12 15:03:14 -0700

    Initial commit

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('home');
+  res.render('DataByAgeDistribution/SelectFeature');
 });
 
 router.get('/response', (req, res) => {
