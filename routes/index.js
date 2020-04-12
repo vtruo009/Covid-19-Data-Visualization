fb816feea96471ec35d87d3d4850089ca82de45f
Johan Guzman <jguzm022@ucr.edu>
2020-04-12 11:27:22 -0700

    Demo video commmit

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('home', { title: 'COVID-19 Website' });
+  res.render('home');
+});
+
+router.get('/response', (req, res) => {
+  console.log("Request received from client");
+  // perform some logic
+  res.render('home', { response: 'Hello From Server!'});
 });
 
 module.exports = router;
