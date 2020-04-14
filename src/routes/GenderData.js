007b88e8af53978f27c1116a36fe82c8fa5a52bc
Mari-Hayashi <mhaya010@ucr.edu>
2020-04-14 14:37:19 -0700

    Moved ReadCSV in another file

--- src/routes/GenderData.js
+++ src/routes/GenderData.js
@@ -6,14 +6,6 @@ router.get('/', (req, res) => {
   res.render('DataByGender/SelectFeature.html')
 });
 
-var parsedCSV = readCSV('../src/database/COVID19_line_list_data.csv');
-
-for (var i = 0; i < parsedCSV.length; ++i){
-    for (var j = 0; j < parsedCSV[0].length; ++j){
-        console.log("Line " + i + ": Cell " + j + ": " + parsedCSV[i][j]);
-    }
-}
-
 // Get request. query paraemeters contain data from form. render search.html passing data
 router.get('/SelectFeature', (req, res) => {
     console.log(req.query);
@@ -53,41 +45,3 @@ router.get('/SelectFeature', (req, res) => {
 });
 
 module.exports = router;
-
-///////////////////// HELPER FUNCTION //////////////////////////////
-
-function readCSV(fileName){
-    const fs = require('fs') 
-  
-    var data = fs.readFileSync(fileName, 'utf8');
-
-    var lines = data.split("\n");
-
-    var parsedCSV = [];
-    lines.forEach(line => splitLine(line));
-
-    function splitLine (line){
-        if (line.length == 0) return;
-
-        var csvArray = [];
-        var curPos = -1;
-        while (curPos < line.length){
-            if (line.charAt(curPos + 1) == '\"'){
-                var endPos = curPos + 2;
-                while (endPos < line.length && line.charAt(endPos) != '"') endPos ++;
-                var cell = line.substring(curPos + 2, endPos);
-                csvArray.push(cell);
-                curPos = endPos + 1;
-            } else {
-                var endPos = curPos + 1;
-                while (endPos < line.length && line.charAt(endPos) != ',') endPos ++;
-                var cell = line.substring(curPos + 1, endPos);
-                csvArray.push(cell);
-                curPos = endPos;
-            }  
-        }
-        parsedCSV.push(csvArray);
-    }
-
-    return parsedCSV;
-}
