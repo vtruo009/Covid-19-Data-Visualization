3acd46b4583e4d515aaad1783e3b411da068b944
Mari-Hayashi <mhaya010@ucr.edu>
2020-04-13 21:59:23 -0700

    Implementing java program to read csv file

--- src/routes/GenderData.js
+++ src/routes/GenderData.js
@@ -6,6 +6,55 @@ router.get('/', (req, res) => {
   res.render('DataByGender/SelectFeature.html')
 });
 
+const fs = require('fs') 
+  
+var data = '';
+
+fs.readFile('../src/database/COVID19_line_list_data.csv', (err, data) => { 
+    if (err) throw err; 
+});
+
+console.log(fs.data);
+var lines = data.split("\n");
+console.log(lines.length);
+
+var cells = [];
+lines.forEach(line => splitLine(line));
+
+function splitLine (line){
+    console.log(line);
+    var curPos = 0;
+    console.log(line.length);
+    while (curPos < line.length){
+        while (curPos < line.length && line[curPos] == ' ') curPos ++;
+
+        if (line[curPos] == '\"'){
+            var endPos = curPos + 1;
+            while (endPos < line.length && endPos != '\"') endPos ++;
+            var cell = line.substr(curPos, endPos);
+            cells.push(cell);
+            curPos = endPos + 2;
+        } else {
+            var endPos = curPos + 1;
+            while (endPos < line.length && endPos != ',') endPos ++;
+            var cell = line.substr(curPos, endPos);
+            cells.push(cell);
+            curPos = endPos + 2;
+        }  
+    }
+}
+
+for (var i = 0; i < 10; ++i){
+    console.log(i + ":   " + cells[i]);
+}
+/*
+    1. var array
+For each character in the file:
+    2. If I see quotation mark, I move to the end quotation mark (ignoring all commas between) 
+        and push it onto the array.
+    3. If I don't see quotation mark, I move to the next comma, and push it onto the array.
+*/
+
 // Get request. query paraemeters contain data from form. render search.html passing data
 router.get('/SelectFeature', (req, res) => {
     console.log(req.query);
