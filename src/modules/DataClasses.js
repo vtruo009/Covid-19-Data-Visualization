b8f6d3daca97c3b5096ec647ab5e961f67568427
Mari-Hayashi <mhaya010@ucr.edu>
2020-04-16 10:57:01 -0700

    Implementing function to load world data

--- src/modules/DataClasses.js
+++ src/modules/DataClasses.js
@@ -16,5 +16,44 @@ module.exports = {
             this.dead = (line[16] == '1');
             this.recovered = (line[17] == '1');
         }
+    },
+    WorldPlace : class{
+        constructor(state, country) {
+            this.state = state;
+            this.country = country;
+
+            // Dictionary of { <Date, number of Deaths> }
+            this.numDeaths = {};
+
+            // Dictionary of { <Date, number of Confirmed cases> }
+            this.numConfirmed = {};
+
+            // Dictionary of { <Date, number of Recovered cases> }
+            this.numRecovered = {};
+        }
+        addNumDeaths(dates, values){
+            // dates is a list of Date {1/20/20, 1/21/20, 1/22/20, ...}
+            // values contains the number of people died on the corresponding date.
+
+            for (var i = 0; i < dates.length; ++i){
+                this.numDeaths[dates[i]] = values[i];
+            }
+        }
+        addNumConfirmed(dates, values){
+            // dates is a list of Date {1/20/20, 1/21/20, 1/22/20, ...}
+            // values contains the number of confirmed cases on the corresponding date.
+
+            for (var i = 0; i < dates.length; ++i){
+                this.numConfirmed[dates[i]] = values[i];
+            }
+        }
+        addNumRecovered(dates, values){
+            // dates is a list of Date {1/20/20, 1/21/20, 1/22/20, ...}
+            // values contains the number of recovered cases on the corresponding date.
+
+            for (var i = 0; i < dates.length; ++i){
+                this.numRecovered[dates[i]] = values[i];
+            }
+        }
     }
 }
