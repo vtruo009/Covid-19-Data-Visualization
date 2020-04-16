06f45e2caac0e8ba76f573752af8ffecbfddc9c1
Johan Guzman <jguzm022@ucr.edu>
2020-04-15 19:12:04 -0700

    Organizes app.js file and adds morgan to output incoming requests to console (for debugging purposes)

--- src/Server/app.js
+++ src/Server/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const createError = require('http-errors');
 const path = require('path');
+const cors = require('cors');
 const app = express();
+const morgan = require('morgan')
 let bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended: true}));
 
 // Port to listen to
 const port = 5000;
@@ -14,11 +15,21 @@ const USDataRouter = require('./routes/USData');
 const WorldDataRouter = require('./routes/WorldData');
 const AgeDataRouter = require('./routes/AgeData');
 const GenderDataRouter = require('./routes/GenderData');
-const cors = require('cors');
+
+// App set up
+
+// Used to make server decode data from body of requests
+app.use(bodyParser.urlencoded({extended: true}));
+
 // Public assests set up
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Used to output incoming requests in console
+app.use(morgan('dev'));
+
+//Used to make server receive requests from client server
 app.use(cors());
+
 // Used to recognize incoming requests as Json objects
 app.use(express.json());
 
