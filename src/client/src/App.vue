cf27fde9a4ead3e6499d8e82c046ee692e51b5d9
Johan Guzman <jguzm022@ucr.edu>
2020-04-16 17:15:12 -0700

    Crestes NavBar component

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -1,59 +1,18 @@
 <template>
 	<div id="app">
-		<b-navbar toggleable="lg" type="dark" variant="dark">
-			<b-navbar-brand to="/"
-				>COVID 19 - DATA <font-awesome-icon :icon="['fas', 'virus']"
-			/></b-navbar-brand>
-
-			<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-			<b-collapse id="nav-collapse" is-nav>
-				<b-navbar-nav>
-					<b-nav-item class="mr-3" to="/US"
-						>U.S. <font-awesome-icon :icon="['fas', 'flag-usa']"
-					/></b-nav-item>
-					<b-nav-item class="mr-3" to="/World"
-						>World <font-awesome-icon :icon="['fas', 'globe-americas']"
-					/></b-nav-item>
-					<b-nav-item class="mr-3" to="/Gender"
-						>By Gender <font-awesome-icon :icon="['fas', 'venus-mars']"
-					/></b-nav-item>
-					<b-nav-item class="mr-3" to="/Age"
-						>By Age <font-awesome-icon :icon="['fas', 'user-friends']"
-					/></b-nav-item>
-					<b-nav-item-dropdown text="Analytics">
-						<b-dropdown-item href="">Analytic 1</b-dropdown-item>
-						<b-dropdown-item href="">Analytic 2</b-dropdown-item>
-						<b-dropdown-item href="">Analytic 3</b-dropdown-item>
-						<b-dropdown-item href="">Analytic 4</b-dropdown-item>
-					</b-nav-item-dropdown>
-				</b-navbar-nav>
-			</b-collapse>
-		</b-navbar>
+		<NavBar></NavBar>
 		<b-container class="bv-example-row">
 			<router-view />
 		</b-container>
 	</div>
 </template>
 <script>
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-	faVirus,
-	faGlobeAmericas,
-	faFlagUsa,
-	faVenusMars,
-	faUserFriends,
-	faChartBar,
-} from '@fortawesome/free-solid-svg-icons';
+import NavBar from './components/NavBar';
 
-library.add(
-	faVirus,
-	faGlobeAmericas,
-	faFlagUsa,
-	faVenusMars,
-	faUserFriends,
-	faChartBar
-);
-export default {};
+export default {
+	components: {
+		NavBar,
+	},
+};
 </script>
 <style></style>
