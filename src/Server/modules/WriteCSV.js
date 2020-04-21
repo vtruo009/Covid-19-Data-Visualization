e5a1b7e5192490a5737c7bc2a2696ccb47228a02
Mari-Hayashi <mhaya010@ucr.edu>
2020-04-21 15:25:27 -0700

    Implementing function to backup data

--- src/Server/modules/WriteCSV.js
+++ src/Server/modules/WriteCSV.js
@@ -1,21 +1,47 @@
-const write = async () => {
-    const classModule = require('../modules/DataClasses.js');
-    const list = [
-        new classModule.Case(["0", "", "1/20/2020", "",
-            "First confirmed imported COVID-19 pneumonia patient in Shenzhen (from Wuhan): male, 66, shenzheng residence, visited relatives in Wuhan on 12/29/2019, symptoms onset on 01/03/2020, returned to Shenzhen and seek medical care on 01/04/2020, hospitalized on 01/11/2020, sample sent to China CDC for testing on 01/18/2020, confirmed on 01/19/2020. 8 others under medical observation, contact tracing ongoing.",
-            "Shenzhen, Guangdong", "China", "male", "66", "01/03/20", "0", "01/11/20", "12/29/2019", "01/04/20", "1", "0", "0", "0"]),
+function write(){
+    const fs = require('fs');
+    var contents = "\"doppo\", 1/20/20, \"Tokyo, Japan\"";
+    fs.writeFileSync("test.txt", contents);
+    console.log("Write on file");
+}
 
-        new classModule.Case(["0", "", "1/20/2020", "",
-            "First confirmed imported COVID-19 pneumonia patient in Shenzhen (from Wuhan): male, 66, shenzheng residence, visited relatives in Wuhan on 12/29/2019, symptoms onset on 01/03/2020, returned to Shenzhen and seek medical care on 01/04/2020, hospitalized on 01/11/2020, sample sent to China CDC for testing on 01/18/2020, confirmed on 01/19/2020. 8 others under medical observation, contact tracing ongoing.",
-            "Shenzhen, Guangdong", "China", "male", "66", "01/03/20", "0", "01/11/20", "12/29/2019", "01/04/20", "1", "0", "0", "0"])
-    ];
+function RecordCases(updatedAllCases) {
+    const readCSVModule = require('../modules/ReadCases.js');
+    var allCases = readCSVModule.loadAllCases();
 
-    const ObjectsToCsv = require('objects-to-csv')
-    const csv = new ObjectsToCsv(list)
-    await csv.toDisk('./database/test.csv')
-    console.log("File is created somewhere.");
+    var labels = allCases[0];
+
+    const fs = require('fs');
+    fs.writeFileSync("/database/COVID19_line_list_data.txt", contents);
+}
+
+function RecordUSData(updatedUSData) {
+    const readCSVModule = require('../modules/ReadCases.js');
+    var allCases = readCSVModule.loadAllCases();
+
+    var labels = allCases[0];
+
+    const fs = require('fs');
+    fs.writeFileSync("/database/COVID19_line_list_data.txt", contents);
+
+    fs.writeFileSync("/database/time_series_covid_19_confirmed_US.txt", contents);
+    fs.writeFileSync("/database/time_series_covid_19_deaths_US.txt", contents);
+}
+
+function RecordWorldData(updatedWorldData) {
+    const readCSVModule = require('../modules/ReadCases.js');
+    var allCases = readCSVModule.loadAllCases();
+
+    var labels = allCases[0];
+
+    const fs = require('fs');
+    fs.writeFileSync("/database/time_series_covid_19_confirmed.txt", contents);
+    fs.writeFileSync("/database/time_series_covid_19_deaths.txt", contents);
+    fs.writeFileSync("/database/time_series_covid_19_recovered.txt", contents);
 }
 
 module.exports = {
-    write : write
+    RecordCases: RecordCases,
+    RecordUSData: RecordUSData,
+    RecordWorldData: RecordWorldData
 };
