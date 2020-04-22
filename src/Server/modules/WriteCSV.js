a6b07b143a6081478b260fd1c14fe46b61ba7a69
Mari-Hayashi <mhaya010@ucr.edu>
2020-04-22 07:57:19 -0700

    Implemented a function to overwrite Cases csv with new data.

--- src/Server/modules/WriteCSV.js
+++ src/Server/modules/WriteCSV.js
@@ -1,47 +1,64 @@
-function write(){
-    const fs = require('fs');
-    var contents = "\"doppo\", 1/20/20, \"Tokyo, Japan\"";
-    fs.writeFileSync("test.txt", contents);
-    console.log("Write on file");
-}
-
 function RecordCases(updatedAllCases) {
-    const readCSVModule = require('../modules/ReadCases.js');
-    var allCases = readCSVModule.loadAllCases();
+    var contents = Cell("id") + // 1
+        Cell("case_in_country") + // 2
+        Cell("reporting date") + // 3
+        Cell("") + // 4
+        Cell("summary") + // 5
+        Cell("location") + // 6
+        Cell("country") + // 7
+        Cell("gender") + // 8
+        Cell("age") + // 9
+        Cell("symptom_onset") + // 10
+        Cell("If_onset_approximated") + // 11
+        Cell("hosp_visit_date") + // 12
+        Cell("exposure_start") + // 13
+        Cell("exposure_end") + // 14
+        Cell("visiting Wuhan") + // 15
+        Cell("from Wuhan") + // 16
+        Cell("death") + // 17
+        Cell("recovered") + // 18
+        Cell("symptom") + // 19
+        Cell("source") + // 20
+        Cell("link"); // 21
 
-    var labels = allCases[0];
+    contents += "\n";
+   
+    updatedAllCases.forEach(appendToCSV);
+    function appendToCSV(item) {
 
+        contents += Cell(item.id) + // 1
+            Cell(item.caseInCountry) + // 2
+            Cell(item.reportingDateStr) + // 3
+            Cell("") + // 4
+            Cell(item.summary) + // 5
+            Cell(item.location) + // 6
+            Cell(item.country) + // 7
+            Cell(item.gender) + // 8
+            Cell(item.age) + // 9
+            Cell(item.symptomOnset) + // 10
+            Cell(item.ifOnsetApproximated) + // 11
+            Cell(item.hospitalVisitDate) + // 12
+            Cell(item.exposureStart) + // 13
+            Cell(item.exposureEnd) + // 14
+            Cell(item.visitingWuhan) + // 15
+            Cell(item.fromWuhan) + // 16
+            Cell(item.dead ? 1 : 0) + // 17
+            Cell(item.recovered ? 1 : 0) + // 18
+            Cell(item.symptom) + // 19
+            Cell(item.source) + // 20
+            Cell(item.link); // 21
+        contents += "\n";
+    }
     const fs = require('fs');
-    fs.writeFileSync("/database/COVID19_line_list_data.txt", contents);
+
+    const fileNameModule = require('../modules/DatabaseFileNames.js');
+    fs.writeFileSync(fileNameModule.CasesFileName, contents);
 }
 
-function RecordUSData(updatedUSData) {
-    const readCSVModule = require('../modules/ReadCases.js');
-    var allCases = readCSVModule.loadAllCases();
-
-    var labels = allCases[0];
-
-    const fs = require('fs');
-    fs.writeFileSync("/database/COVID19_line_list_data.txt", contents);
-
-    fs.writeFileSync("/database/time_series_covid_19_confirmed_US.txt", contents);
-    fs.writeFileSync("/database/time_series_covid_19_deaths_US.txt", contents);
-}
-
-function RecordWorldData(updatedWorldData) {
-    const readCSVModule = require('../modules/ReadCases.js');
-    var allCases = readCSVModule.loadAllCases();
-
-    var labels = allCases[0];
-
-    const fs = require('fs');
-    fs.writeFileSync("/database/time_series_covid_19_confirmed.txt", contents);
-    fs.writeFileSync("/database/time_series_covid_19_deaths.txt", contents);
-    fs.writeFileSync("/database/time_series_covid_19_recovered.txt", contents);
+function Cell(content) {
+    return "\"" + content + "\",";
 }
 
 module.exports = {
-    RecordCases: RecordCases,
-    RecordUSData: RecordUSData,
-    RecordWorldData: RecordWorldData
+    RecordCases: RecordCases
 };
