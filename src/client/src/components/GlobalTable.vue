341b20ac8296bc88b63099d37ec9c8db41a4c59a
Johan Guzman <jguzm022@ucr.edu>
2020-04-22 18:13:54 -0700

    Progress, each row displays two dummy buttons

--- src/client/src/components/GlobalTable.vue
+++ src/client/src/components/GlobalTable.vue
@@ -1,15 +1,42 @@
 <template>
-	<div>
-		<b-table dark striped hover :items="this.data"> </b-table>
-	</div>
+  <div>
+    <b-table dark striped hover :items="this.data" :fields="fields">
+      <template v-slot:cell(date)="row">{{row.value}}</template>
+      <template v-slot:cell(number)="row">{{row.value}}</template>
+      <template v-slot:cell(actions)="row">
+        <b-button size="sm" @click class="mr-1">Info modal</b-button>
+        <b-button size="sm" @click>Details</b-button>
+      </template>
+    </b-table>
+  </div>
 </template>
 
 <script>
 export default {
-	name: 'GlobalTable',
-	props: {
-		data: Array,
-	},
+  name: "GlobalTable",
+  props: {
+    data: Array
+  },
+  data() {
+    return {
+      fields: [
+        { key: "date", label: "Date" },
+        { key: "numberOfConfirmed", label: "Number" },
+        { key: "actions", label: "Actions" }
+      ],
+      items: [
+        {
+          text: "booo",
+          noo: "booo",
+          x: "booo",
+          c: "booo",
+          nx: "booo",
+          update: "<button> PENCIL </button>",
+          delete: "<button> TRASH CAN </button>"
+        }
+      ]
+    };
+  }
 };
 </script>
 
