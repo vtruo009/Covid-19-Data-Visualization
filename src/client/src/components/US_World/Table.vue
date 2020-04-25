144a5801d885e91efcea8131ebc80372ec2834d4
Johan Guzman <jguzm022@ucr.edu>
2020-04-24 22:02:23 -0700

    Adds space between spinner and Loading message

--- src/client/src/components/US_World/Table.vue
+++ src/client/src/components/US_World/Table.vue
@@ -11,7 +11,7 @@
 			<template v-slot:table-busy>
 				<div class="text-center text-danger my-2">
 					<b-spinner class="align-middle"></b-spinner>
-					<strong>Loading...</strong>
+					<strong> Loading...</strong>
 				</div>
 			</template>
 			<template v-slot:cell(date)="row">{{ row.value }}</template>
