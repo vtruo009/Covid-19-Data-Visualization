4e3bfd353fa07529cbf95dfe4725c083dcb47cba
vtruo009 <43657607+vtruo009@users.noreply.github.com>
2020-04-25 19:58:42 -0700

    Fixed name error

--- src/Server/routes/WorldData.js
+++ src/Server/routes/WorldData.js
@@ -87,7 +87,7 @@ router.get('/search', (req, res) => {
 
 router.post('/delete', (req, res) => {
 	res.send({
-		success: ModModule.WorldDelete(req.body.Country, req.body.State, req.body.date, WorldData, req.body.TypeOfData) //lower or upper case date?
+		success: ModModule.DeleteWorldData(req.body.Country, req.body.State, req.body.date, WorldData, req.body.TypeOfData) //lower or upper case date?
 	})
 })
 module.exports = router;
