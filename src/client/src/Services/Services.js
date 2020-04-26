f573c1247e4c615ceca8dd785ce73ac302ecb25d
Johan Guzman <jguzm022@ucr.edu>
2020-04-26 10:23:19 -0700

    Comment out dummy returns

--- src/client/src/Services/Services.js
+++ src/client/src/Services/Services.js
@@ -3,39 +3,39 @@ import API from './API';
 export default {
 	// calls search endpoint of any of the routes in the backend server
 	searchData(payload) {
-		// return API()
-		// 	.get(`${payload.apiEndPoint}/search`, {
-		// 		params: payload.params,
-		// 	})
-		// 	.then((response) => response)
-		// 	.catch((error) => error.status);
 		console.log(payload);
-		return {
-			success: false,
-			ReportingDate: '01/20/20',
-			Country: 'Peru',
-			State: 'Lima',
-			Age: 10,
-			Gender: 1,
-			TypeOfCase: 1,
-		};
+		return API()
+			.get(`${payload.apiEndPoint}/search`, {
+				params: payload.params,
+			})
+			.then((response) => response)
+			.catch((error) => error.status);
+		// return {
+		// 	success: false,
+		// 	ReportingDate: '01/20/20',
+		// 	Country: 'Peru',
+		// 	State: 'Lima',
+		// 	Age: 10,
+		// 	Gender: 1,
+		// 	TypeOfCase: 1,
+		// };
 	},
 	// calls insert endpoint of any of the routes in the backend server
 	insertData(payload) {
 		console.log(payload);
-		return {
-			success: true,
-			ReportingDate: '01/20/20',
-			Country: 'Peru',
-			State: 'Lima',
-			Age: 10,
-			Gender: 1,
-			TypeOfCase: 1,
-		};
-		// return API()
-		// 	.post(`${payload.apiEndPoint}/insert`, payload.body)
-		// 	.then((response) => response)
-		// 	.catch((error) => error.status);
+		// return {
+		// 	success: true,
+		// 	ReportingDate: '01/20/20',
+		// 	Country: 'Peru',
+		// 	State: 'Lima',
+		// 	Age: 10,
+		// 	Gender: 1,
+		// 	TypeOfCase: 1,
+		// };
+		return API()
+			.post(`${payload.apiEndPoint}/insert`, payload.body)
+			.then((response) => response)
+			.catch((error) => error.status);
 	},
 	// calls update endpoint of any of the routes in the backend server
 	updateData(payload) {
@@ -52,10 +52,10 @@ export default {
 		// return {
 		// 	success: true,
 		// };
-		// Implements create request to server
-		// return API()
-		// 	.post(`${payload.apiEndPoint}/delete`, payload.body)
-		// 	.then((response) => response)
-		// 	.catch((error) => error.status);
+		//Implements create request to server
+		return API()
+			.post(`${payload.apiEndPoint}/delete`, payload.body)
+			.then((response) => response)
+			.catch((error) => error.status);
 	},
 };
