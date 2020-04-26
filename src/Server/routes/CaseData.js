caa3971ce94bdbe39b8c4a85f92d965e5dc45ced
Johan Guzman <jguzm022@ucr.edu>
2020-04-26 12:39:21 -0700

    More comments

--- src/Server/routes/CaseData.js
+++ src/Server/routes/CaseData.js
@@ -9,8 +9,20 @@ router.get('/search', (req, res) => {
 	//}
 
 	// NOTE: This router receives a GET request. You can access data through the query of the request. Ex: req.query.someData
-	// returns dummy data
+
+	// Should return a response object with the following structure
+	// {
+	//   CaseId: 12              // integer
+	//   Country: "United State" // string
+	//   State: "California"     // string
+	//   Gender: "1"             // string  "1" => male, "2" => female
+	//   TypeOfCase: "2"         // string  "1" => dead, "2" => recovered
+	//   Age: 10                 // integer
+	//   ReportingDate: "01/20/20" // String
+	//}
+
 	res.send({
+		// returns dummy data for example purposes
 		dummyDate: 'hello',
 	});
 });
@@ -28,8 +40,14 @@ router.post('/update', (req, res) => {
 	//}
 
 	// NOTE: This router receives a POST request. You can access data through the body of the request. Ex: req.body.someData
-	// returns dummy data
+
+	// Should return a response object with the following structure
+	//{
+	//    success: true => // boolean => depending on whether the deletion was sucessful or not
+	//}
+
 	res.send({
+		// returns dummy data for example purposes
 		dummyDate: 'hello',
 	});
 });
@@ -47,8 +65,13 @@ router.post('/insert', (req, res) => {
 	//}
 
 	// NOTE: This router receives a POST request. You can access data through the body of the request. Ex: req.body.someData
-	// returns dummy data
+
+	// Should return a response object with the following structure
+	//{
+	//    success: true => // boolean => depending on whether the deletion was sucessful or not
+	//}
 	res.send({
+		// returns dummy data for example purposes
 		dummyDate: 'hello',
 	});
 });
@@ -62,8 +85,12 @@ router.post('/delete', (req, res) => {
 
 	// NOTE: This router receives a POST request. You can access data through the body of the request. Ex: req.body.caseId
 
-	// returns dummy data
+	// Should return a response object with the following structure
+	//{
+	//    success: true => // boolean => depending on whether the deletion was sucessful or not
+	//}
 	res.send({
+		// returns dummy data for example purposes
 		dummyDate: 'hello',
 	});
 });
