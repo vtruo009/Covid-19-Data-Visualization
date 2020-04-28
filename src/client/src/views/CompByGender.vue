99225b1ccf81ffdf93fad6ecf863e07e290306cd
vtruo009 <43657607+vtruo009@users.noreply.github.com>
2020-04-28 00:05:39 -0700

    Set up new analytics pages

---
+++ src/client/src/views/CompByGender.vue
@@ -0,0 +1,15 @@
+<template>
+  <div> 
+    <h1> Hello from compare by gender </h1> 
+  </div>
+</template>
+
+<script>
+export default {
+    name: "CompByGender"
+}
+</script>
+
+<style>
+
+</style>
