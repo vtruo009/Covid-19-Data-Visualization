1daf4f2263c82aa6e4d90295bc072fcd59309b72
Mari-Hayashi <mhaya010@ucr.edu>
2020-04-28 11:56:35 -0700

    Resolved comment.

--- src/Server/modules/Analytics.js
+++ src/Server/modules/Analytics.js
@@ -3,34 +3,27 @@ const readCSVModule = require('../modules/ReadCSV.js');
 function GetGenderAnalytics(country, typeOfData) {
     var allCases = readCSVModule.LoadAllCases();
 
-    var femaleNum = 0;
-    var maleNum = 0;
+    var female = 0;
+    var male = 0;
 
     for (var i = 0; i < allCases.length; ++i) {
         if (allCases[i].country == country) {
             if (allCases[i].gender == "female") {
-                if (typeOfData == 2 && allCases[i].dead) femaleNum++; // dead
-                else if (typeOfData == 3 && allCases[i].recovered) femaleNum++; //recovered
-                else if (typeOfData == 1) femaleNum++; // confirmed
+                if (typeOfData == 2 && allCases[i].dead) female++; // dead
+                else if (typeOfData == 3 && allCases[i].recovered) female++; //recovered
+                else if (typeOfData == 1) female++; // confirmed
             }
             else if (allCases[i].gender == "male") {
-                if (typeOfData == 2 && allCases[i].dead) maleNum++; // dead
-                else if (typeOfData == 3 && allCases[i].recovered) maleNum++; //recovered
-                else if (typeOfData == 1) maleNum++; // confirmed
+                if (typeOfData == 2 && allCases[i].dead) male++; // dead
+                else if (typeOfData == 3 && allCases[i].recovered) male++; //recovered
+                else if (typeOfData == 1) male++; // confirmed
             }
         }
     }
 
-    if (femaleNum == 0 && maleNum == 0) return undefined;
+    if (female == 0 && male == 0) return undefined;
 
-    return new GenderAnalytics(femaleNum, maleNum);
-}
-
-class GenderAnalytics {
-    constructor(femaleNum, maleNum) {
-        this.femaleNum = femaleNum; // integer
-        this.maleNum = maleNum; // integer
-    }
+    return { femaleNum: female, maleNum: male }
 }
 
 module.exports = {
