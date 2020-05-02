6d50d31693ca0f220f732178948f0e070356d70f
Johan Guzman <jguzm022@ucr.edu>
2020-05-01 19:00:20 -0700

    Form submission does not reload page

--- src/client/src/views/CompByGender.vue
+++ src/client/src/views/CompByGender.vue
@@ -1,15 +1,69 @@
 <template>
-  <div> 
-    <h1> Hello from compare by gender </h1> 
-  </div>
+	<div class="mt-5">
+		<!-- By default the first search looka for confirmed cases -->
+		<b-form inline @submit="initialFormSubmission">
+			<b-input v-model="Country" required placeholder="Country"></b-input>
+			<b-button type="submit" variant="primary" class="ml-3"> Search </b-button>
+		</b-form>
+
+		<div class="mt-5">
+			<DonutChart v-bind:data="chartData" />
+		</div>
+	</div>
 </template>
 
 <script>
+import Services from '../Services/Services';
+import DonutChart from '../components/DonutChart';
 export default {
-    name: "CompByGender"
-}
+	name: 'CompByGender',
+	data() {
+		return {
+			Country: null,
+			chartData: [
+				['Task', 'Hours per Day'],
+				['Male', 2],
+				['Female', 11],
+			],
+		};
+	},
+	methods: {
+		// 1-> confirmed, 2-> dead, 3->recovered
+		initialFormSubmission(e){
+			e.preventDefault();
+			//this.GetData('1')
+			console.log("Form submitted");
+		},
+		async GetData(TypeOfData) {
+			console.log("Getting data");
+			try {
+				const response = await Services.GetAnalyticsData({
+					apiEndPoint: '/compareGender',
+					params: {
+						Country: this.Country,
+						TypeOfData: TypeOfData,
+					},
+				});
+				if (response.data.CountryExists) {
+					// Set up chartData object to visualize the donut chart
+					this.chartData = [
+						['Gender', 'Number of Cases'],
+						['Male', this.response.data.MaleNumberOfCases],
+						['Female', this.response.data.FemaleNumberOfCases],
+					];
+				} else {
+					// TO DO: SHOW ERROR: "Country does not exist"
+				}
+			} catch (error) {
+				// TO DO: SHOW ERROR: "Some error occurred. Please try again"
+				console.log(error);
+			}
+		},
+	},
+	components: {
+		DonutChart,
+	},
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
