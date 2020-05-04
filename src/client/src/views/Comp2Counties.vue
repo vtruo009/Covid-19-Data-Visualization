d0dd73331814d21709180eacb0ad6c5f3de55769
root <root@DESKTOP-0D26ARA.localdomain>
2020-05-03 17:51:45 -0700

    Compare within two counties

--- src/client/src/views/Comp2Counties.vue
+++ src/client/src/views/Comp2Counties.vue
@@ -1,22 +1,127 @@
 <template>
-  <div class="Comp2Countries fadeIn">
-    <div>
-      <h3>Please Enter The Information Below</h3>
+  <div class="mt-5">
+    <b-form inline @submit="initialFormSubmission">
+      <b-input v-model="State1" required placeholder="Province/State1"></b-input>
+      <b-input class="ml-3" v-model="County1" required placeholder="County1"></b-input>
+      <b-input class="ml-5" v-model="State2" required placeholder="Province/State2"></b-input>
+      <b-input class="ml-3" v-model="County2" required placeholder="County2"></b-input>
+      <b-button type="submit" variant="primary" class="ml-5">Search</b-button>
+    </b-form>
+
+    <div v-if="showChart" class="mt-5">     <!-- this will only render if showChart is a truthy value -->
+      <b-row>     <!-- row contains donut chart & selection -->
+        <b-col>
+          <DonutChart v-bind:data="chartData"/> <!-- will display whatever bind to it -->
+        </b-col>
+        <b-col>
+          <!-- at chagne, calls requestFromSelect func
+              displays typeofdataoptions listed in data()  -->
+          <b-form-select
+            @change="requestFromSelect"
+            v-model="TypeOfData"
+            required
+            :options="TypeOfDataOptions"
+          ></b-form-select>
+        </b-col>
+      </b-row>
     </div>
-    <Body firstInputName="Country1" secondInputName="State1" thirdInputName="Country2" fourthInputName="State2" apiEndPoint="/Comp2Countries" />
   </div>
 </template>
 
 <script>
-import Body from "../components/Analy_Compare/Body";
+import Services from "../Services/Services";
+import DonutChart from "../components/DonutChart";
+
 export default {
     name: "Comp2Counties",
-    components: {
-      Body
+    data() {
+      return {
+        //Changes according to the input field (user inputs)
+        State1: null,
+        County1: null,
+        State2: null,
+        County2: null,
+        TypeOfData: null,
+        TypeOfDataOptions: [
+          { value: null, text: 'Type of data', disabled: true },
+          { value: '1', text: 'Confirmed' },
+          { value: '2', text: 'Dead' },
+          { value: '3', text: 'Recovered' },
+        ],
+        chartData: null,
+        showChart: false,
+
+        // error: false,
+			  // errorMessage: null,
+      //   cacheFirstInput: null,
+			//  cacheSecondInput: null,
+			// cacheTypeOfData: null,
+			// cacheTypeOFDataString: null,
+      };
     },
+    methods: {
+      // 1-> confirmed, 2-> dead, 3->recovered
+      async initialFormSubmission(e) {
+        e.preventDefault();
+        this.TypeOfData = null;
+        await this.GetData('1'); //first submit gets confirmed as default
+        console.log("Information Submitted!");
+      },
+      async requestFromSelect() {
+        await this.GetData(this.TypeOfData); //whatever user chooses
+        console.log("getting what user selected");
+      },
+      async GetData(TypeOfData) {
+        console.log("IN GETDATA()");
+        try {
+          const response = await Services.GetAnalyticsData({
+            apiEndPoint: '/compareCounties',
+            params: {
+              State1: this.State1,
+              County1: this.County1,
+              State2: this.State2,
+              County2: this.County2,
+              TypeOfData: TypeOfData,
+            },
+          });
+          if (response.data.County1Exists && response.data.County2Exists) {
+            if (!this.showChart) this.showChart = true;
+            this.chartData = [
+              ['County', 'Number of Cases'],
+              [this.County1, response.data.County1NumberOfCases],
+              [this.County2, response.data.County2NumberOfCases],
+            ];
+          }
+          else {
+            if (!response.data.County1Exists) {
+              this.erroHandler(
+                this.County1 + ", " + this.State1 + " does not exist. Please try a different province & county"
+              );
+            }
+            else if (!response.data.County2Exists) {
+              this.erroHandler(
+                this.County2 + ", " + this.State2 + " does not exist. Please try a different province & county"
+              );
+            }
+         }
+      }
+        catch(error) {
+          this.erroHandler('Some error occurred. Please try again later');
+				  console.log(error);
+        }
+      },
+      erroHandler(message) {
+        this.$toast.error(message, {
+          position: 'bottom-right',
+          timeout: 3500,
+        });
+      },
+    },
+
+    components: {
+      DonutChart,
+    }
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
