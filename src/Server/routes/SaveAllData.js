f73b5d2a24ca4cf907913b4679a53d8ec61e2a14
root <root@DESKTOP-0D26ARA.localdomain>
2020-05-15 19:31:03 -0700

    Modify SaveAllData.js

--- src/Server/routes/SaveAllData.js
+++ src/Server/routes/SaveAllData.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
-
+const ModUSPlacesManager = require('../modules/USPlacesManager.js');
 router.post('/', (req, res) => {
 	console.log('Client session was closed');
 	// TO DO: Save all data in the respective csv files.
+	res.send({
+		success: ModUSPlacesManager.SaveRecords(),
+	});
 });
 
 module.exports = router;
