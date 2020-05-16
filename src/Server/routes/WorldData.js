b7a17a3dbc2c18d59c4cfd36faa94513330fcd1c
Van Truong <vantruong@Vans-iMac.local>
2020-05-15 23:24:06 -0700

    Changed all functions to WorldPlaceManager functions

--- src/Server/routes/WorldData.js
+++ src/Server/routes/WorldData.js
@@ -1,109 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-const ModModule = require('../modules/ModifyData.js');
+const WP = require('../modules/WorldPlaceManager.js');
 
-const readCSVModule = require('../modules/ReadCSV.js');
-var WorldData = readCSVModule.LoadWorldData();
-
-const worldReq = require('../modules/DataClasses.js');
 router.get('/search', (req, res) => {
-	console.log(req.query);
-	// Get respective data using the query parameters
-	var selectedCountry = [];
-	// Now, WorldData stores list of WorldPlace read from the csv file.
-
-	// console.log(WorldData);
-	for (var i = 0; i < WorldData.length; ++i) {
-		if (
-			req.query.Country == WorldData[i].country &&
-			req.query.State == WorldData[i].state
-		) {
-			selectedCountry.push(WorldData[i]);
-		}
-	}
-
-	console.log(selectedCountry);
-
-	if (selectedCountry.length > 0) {
-		if (req.query.TypeOfData == 1) {
-			var row = [];
-			for (var key in selectedCountry[0].numConfirmed) {
-				var temp_date = new Date(key);
-				var date =
-					temp_date.getMonth() +
-					1 +
-					'/' +
-					temp_date.getDate() +
-					'/' +
-					temp_date.getFullYear();
-				var newItem = new worldReq.WorldRowConfirmed(
-					date,
-					selectedCountry[0].numConfirmed[key]
-				);
-				row.push(newItem);
-			}
-		} else if (req.query.TypeOfData == 2) {
-			var row = [];
-			for (var key in selectedCountry[0].numDeaths) {
-				var temp_date = new Date(key);
-				var date =
-					temp_date.getMonth() +
-					1 +
-					'/' +
-					temp_date.getDate() +
-					'/' +
-					temp_date.getFullYear();
-				var newItem = new worldReq.WorldRowDeaths(
-					date,
-					selectedCountry[0].numDeaths[key]
-				);
-				row.push(newItem);
-			}
-		} else if (req.query.TypeOfData == 3) {
-			var row = [];
-			for (var key in selectedCountry[0].numRecovered) {
-				var temp_date = new Date(key);
-				var date =
-					temp_date.getMonth() +
-					1 +
-					'/' +
-					temp_date.getDate() +
-					'/' +
-					temp_date.getFullYear();
-				var newItem = new worldReq.WorldRowRecovered(
-					date,
-					selectedCountry[0].numRecovered[key]
-				);
-				row.push(newItem);
-			}
-		}
-	}
-
 	res.send({
-		data: row,
+		data: WP.GetRows( req.query.Country, req.query.State, req.query.TypeOfData), // call GetRow() samething for delete update insert
 	});
 });
 
 router.post('/delete', (req, res) => {
 	res.send({
-		success: ModModule.DeleteWorldData(
-			req.body.Country,
+		success: WP.RemoveWorldData(
 			req.body.State,
+			req.body.Country,
 			req.body.Date,
-			WorldData,
-			req.body.TypeOfData
-		),
+			req.body.TypeOfData),
 	});
 });
 
 router.post('/update', (req, res) => {
 	res.send({
-		success: ModModule.UpdateWorldData(
-			req.body.Country,
+		success: WP.EditWorldData(
 			req.body.State,
+			req.body.Country,
 			req.body.Date,
-			WorldData,
 			req.body.TypeOfData,
 			req.body.Number
 		),
@@ -111,11 +32,10 @@ router.post('/update', (req, res) => {
 });
 
 router.post('/insert', (req, res) => {
-	var msg = ModModule.InsertWorldData(
-		req.body.Country,
+	var msg = WP.AddWorldData(
 		req.body.State,
+		req.body.Country,
 		req.body.Date,
-		WorldData,
 		req.body.TypeOfData,
 		req.body.Number
 	);
