bbe86c3def798d7e1d70c0d945ee7db52158d7f2
Van Truong <vantruong@Vans-iMac.local>
2020-05-15 23:01:54 -0700

    Added SaveRecords() to SaveAllData

--- src/Server/routes/SaveAllData.js
+++ src/Server/routes/SaveAllData.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
+const WorldPlace = require('../modules/WorldPlaceManager.js');
 
 router.post('/', (req, res) => {
 	console.log('Client session was closed');
 	// TO DO: Save all data in the respective csv files.
+	WorldPlace.SaveRecords();
 });
 
 module.exports = router;
